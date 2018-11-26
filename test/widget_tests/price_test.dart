b0769e0438d44b264af936063289cf984b19ee65
Wekra <wech1034@hs-karlsruhe.de>
2018-11-26 20:49:46 +0100

    Fix widget test

--- test/widget_tests/price_test.dart
+++ test/widget_tests/price_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:service_app/data/model/part.dart';
 import 'package:service_app/widgets/price.dart';
@@ -5,14 +6,14 @@ import 'package:service_app/widgets/price.dart';
 void main() {
   testWidgets('Create Price Widget with Euro price',
       (WidgetTester tester) async {
-    await tester.pumpWidget(new PriceWidget(354, Currency.eur));
+    await tester.pumpWidget(new PriceWidget(354, Currency.eur, TextStyle()));
 
     expect(find.text('Price: 3.54€'), findsOneWidget);
   });
 
   testWidgets('Create Price Widget with Dollar price',
       (WidgetTester tester) async {
-    await tester.pumpWidget(new PriceWidget(354, Currency.usd));
+    await tester.pumpWidget(new PriceWidget(354, Currency.usd, TextStyle()));
 
     expect(find.text('Price: \$3.54'), findsOneWidget);
   });
