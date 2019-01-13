bc9017288373f0aefeab7928d137c00d10a279c9
matthias-baeuerle <Ma.Baeuerle@gmx.de>
2019-01-13 11:33:46 +0100

    Change CustomerDetailPage to regular StatelessWidget

--- lib/screens/customer_detail.dart
+++ lib/screens/customer_detail.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:service_app/data/model/customer.dart';
-import 'package:service_app/screens/drawer_page.dart';
 import 'package:service_app/widgets/customer_appointments.dart';
 import 'package:service_app/widgets/customer_datatab.dart';
 
-class CustomerDetailPage extends DrawerPage {
+class CustomerDetailPage extends StatelessWidget {
   final Customer _customer;
 
   CustomerDetailPage(this._customer);
-
-  @override
-  String get title => "Customer: ${_customer.name}";
-
-  @override
-  IconData get icon => Icons.people;
-
-  @override
-  _CustomerDetailPageState createState() {
-    return _CustomerDetailPageState(_customer);
-  }
-}
-
-class _CustomerDetailPageState extends State<CustomerDetailPage> {
-  final Customer _customer;
-
-  _CustomerDetailPageState(this._customer);
 
   @override
   Widget build(BuildContext context) {
